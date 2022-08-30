810ae8448ccfeb1feb95dec40672d24aa2dc5209
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2022-08-30 04:01:11 -0300

    se agrego buscar todos los objetos Educacion por usuario

--- src/main/java/com/PortfolioCSL/PortfolioV2/repository/IEducacionRepository.java
+++ src/main/java/com/PortfolioCSL/PortfolioV2/repository/IEducacionRepository.java
@@ -2,10 +2,17 @@
 package com.PortfolioCSL.PortfolioV2.repository;
 
 import com.PortfolioCSL.PortfolioV2.model.Educacion;
+import com.PortfolioCSL.PortfolioV2.model.Usuario;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IEducacionRepository extends JpaRepository<Educacion,Long>{
+    
+    @Query("select x from Educacion x where x.usuario like :var_parm")
+    public abstract List<Educacion> listaPorUsuarioId(@Param("var_parm") Usuario usuario);
     
 }
