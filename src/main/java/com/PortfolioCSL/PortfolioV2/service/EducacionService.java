45544dafeeae4c504d4eb760161fbb9da51cc0d3
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2022-08-30 04:01:25 -0300

    se agrego buscar todos los objetos Educacion por usuario

--- src/main/java/com/PortfolioCSL/PortfolioV2/service/EducacionService.java
+++ src/main/java/com/PortfolioCSL/PortfolioV2/service/EducacionService.java
@@ -2,6 +2,7 @@
 package com.PortfolioCSL.PortfolioV2.service;
 
 import com.PortfolioCSL.PortfolioV2.model.Educacion;
+import com.PortfolioCSL.PortfolioV2.model.Usuario;
 import com.PortfolioCSL.PortfolioV2.repository.IEducacionRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,11 @@ public class EducacionService implements IEducacionService{
     @Override
     public void borrarEducacion(Long id) {
          edu.deleteById(id);
+    }
+
+    @Override
+    public List<Educacion> leerEducacionPorUsuario(Usuario usuario) {
+        return edu.listaPorUsuarioId(usuario);
     }
     
 }
